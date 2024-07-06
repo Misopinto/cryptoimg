1d7e1643657a6396379a2272d840cbe918921037
Misopinto <misopintodev@gmail.com>
2024-07-06 10:32:46 +0200

    convert image to array and reverse

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import numpy
+from PIL import Image
+import numpy as np
+
+def imgtoarr(img):
+    imag= Image.open(img)
+    imgarr= np.asarray(imag)
+    return imgarr
+
+def arrtoimg(arr):
+    return Image.fromarray(arr)
+
+if __name__ == "__main__":
+    arr=imgtoarr("cat_lock.jpg")
+    print(arr)
+    img= arrtoimg(arr)
+    img.show()

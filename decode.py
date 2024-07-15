dff1defeefbc6d986896ddaa2ab7e0a9cb93159e
Misopinto <misopintodev@gmail.com>
2024-07-15 13:01:49 +0200

    Update

--- decode.py
+++ decode.py
@@ -1,12 +1,25 @@
+import argparse
 from cryptoimg_func import binTostr,decrypt,imgToarr,save_file
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+parser.add_argument('-e')
+parser.add_argument('-i')
+parser.add_argument('-s')
+
+args = parser.parse_args()
+
+def init_path():
     #Get encrypted file image path
     encrypted_path = input('Insert Encrypted file image path: ')
-    #Convert image to array
-    encrypted = imgToarr(encrypted_path)
     #Get the Image path
     img_path = input('Insert Image path: ')
+    #Save File
+    filepath = input('Insert decrypted file path where to save: ')
+    return encrypted_path,img_path,filepath
+
+def main(encrypted_path,img_path,filepath):
+    #Convert image to array
+    encrypted = imgToarr(encrypted_path)
     #Convert image to array
     img_arr = imgToarr(img_path)
     #Decrypt the File
@@ -14,7 +27,10 @@ if __name__ == '__main__':
     #Convert binary to string
     file = binTostr(binary_file)
     #Save File
-    filepath = input('Insert decrypted file path where to save: ')
-    save_file(filepath,file)
+    save_file(f'{filepath}.txt',file)
 
+if __name__ == '__main__':
+    if args.e == None and args.i == None and args.s == None:
+        args.e,args.i,args.s = init_path()
+    main(args.e,args.i,args.s)
 
--- encode.py
+++ encode.py
@@ -1,12 +1,24 @@
+import argparse
 from cryptoimg_func import arrToimg,encrypt,file_open,imgToarr,reshape,strTobin
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+parser.add_argument('-i')
+parser.add_argument('-f')
+parser.add_argument('-s')
+
+args = parser.parse_args()
+
+def init_path():
     #Get image Path
     img_path = input('Insert image path: ')
-    #initialize image as arr
-    img = imgToarr(img_path)
     #Get file path
     file_path = input('Insert File path: ')
+    encrypted_img_path= input('Insert the path where to save the encrypted image: ')
+    return img_path,file_path,encrypted_img_path
+
+def main(img_path,file_path,encrypted_img_path='Encryptedimg'):
+    #initialize image as arr
+    img = imgToarr(img_path)
     #Read File and convert string to binary
     file = file_open(file_path)
     binary = strTobin(file)
@@ -15,6 +27,9 @@ if __name__ == '__main__':
     #Encrypt the file using the image and a pseudorandomic key
     encrypted = encrypt(binary_reshaped,img,binary_byte)
     encrypted_img = arrToimg(encrypted)
-    #Save the encrypted file as an image
-    encrypted_img_path= input('Insert the path where to save the encrypted image: ')
-    encrypted_img.save(encrypted_img_path,'PNG')
+    encrypted_img.save(f'{encrypted_img_path}.png','PNG')
+
+if __name__ == '__main__':
+    if args.i == None and args.f == None and args.s == None:
+        args.i,args.f,args.s = init_path()
+    main(args.i,args.f,args.s)
